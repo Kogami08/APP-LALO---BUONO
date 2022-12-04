d21834eb78dfaf65f0f162762ba9e31a84da9ab3
Adrian Reza <adrianmetal1011@gmail.com>
2022-12-04 06:30:42 -0600

    LocalStorage Login funcional

---
+++ Buono/www/js/login.js
@@ -0,0 +1,47 @@
+function registrar(){
+   
+    var email = document.getElementById('email').value;
+    var username = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+
+    //agregaremos una validacion para que no queden espacios vacios
+
+    if (username == null || username == ""){
+        alert("Insete un usuario");
+    } else if (email == null || email == ""){
+        alert("Agregar un email por favor")
+    } else if (pass == null || pass == ""){
+        alert("Inserte por favor una contraseña")
+    } else {
+        var user = {
+            email: email,
+            username: username,
+            password: pass,
+        };
+    
+        var json = JSON.stringify(user);
+        localStorage.setItem(username, json);
+        console.log('Usuario agregado exitosamente')
+        location.href='../login.html';
+        
+    }
+}
+
+function iniciar() {
+
+var username = document.getElementById('username').value;
+var pass = document.getElementById('password').value;
+
+var user = localStorage.getItem(username);
+var data = JSON.parse(user);
+console.log(data)
+
+if(user == null || user==""){
+    alert('usuario incorrecto')
+}else if(pass == null || pass == ""){
+    alert('contraseña incorrecta')
+}else{
+    alert('iniciando');
+    location.href='./index.html';
+}
+}
